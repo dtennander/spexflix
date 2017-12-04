8e5cdc16561893fbdf31b0d36470fad9f82e5e43
David Tennander <david.tennander@gmail.com>
2017-12-04 22:49:41 +0100

    Adds working login page.

--- gateway/server/server.go
+++ gateway/server/server.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	"context"
 	"flag"
-	au "github.com/DiTo04/spexflix/authentication"
 	"github.com/DiTo04/spexflix/authentication/api"
-	"golang.org/x/net/context"
+	"github.com/DiTo04/spexflix/infrastructure"
 	"google.golang.org/grpc"
+	"html/template"
+	"io/ioutil"
 	"log"
+	"net/http"
+	"os"
 )
 
 var (
@@ -14,11 +18,66 @@ var (
 )
 
 type server struct {
-	sessions au.SessionPool
+	connections infrastructure.Connections
+	logger      *log.Logger
+}
+
+func (s *server) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
+	switch r.RequestURI {
+	case "/login":
+		s.HandleLogin(rw, r)
+	default:
+		s.logger.Print("Got: " + r.RequestURI)
+	}
+}
+func (s *server) HandleLogin(rw http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case "GET":
+		file, err := ioutil.ReadFile("gateway/login-page.tmpl")
+		if err != nil {
+			s.logger.Fatal("Got error: " + err.Error())
+		}
+		tmpl, err := template.New("Logi").Parse(string(file))
+		if err != nil {
+			s.logger.Fatal("Got error: " + err.Error())
+		}
+		tmpl.Execute(rw, nil)
+	case "POST":
+		err := r.ParseForm()
+		if err != nil {
+			s.logger.Print("Could not parse form, error: " + err.Error())
+			http.Error(rw, "Could not parse form", http.StatusNotAcceptable)
+		}
+		username := r.Form.Get("name")
+		password := r.Form.Get("password")
+		req := &api.LoginRequest{Username: username, Password: password}
+		ctx := context.Background()
+		rsp, err := s.connections.GetAuthenticationClient().Login(ctx, req)
+		switch {
+		case err != nil:
+			s.logger.Print(err.Error())
+			http.Error(rw, "Could not authenticate", http.StatusInternalServerError)
+			break;
+		case !rsp.IsAuthenticated:
+			http.Error(rw, "Invalid credentials", http.StatusNotAcceptable)
+			break;
+		default:
+			s.logger.Print(rsp.SessionToken)
+			cookie := &http.Cookie{
+				Name:     "SessionToken",
+				Path:     "/",
+				Secure:   false,
+				HttpOnly: false,
+				Value:    rsp.SessionToken,
+			}
+			http.SetCookie(rw, cookie)
+			http.Redirect(rw, r, "/browse", http.StatusFound)
+		}
+
+	}
 }
 
 //This server is the gateway onto Spexflix.
-//
 //If you are logged in you get passed to the home-page.
 //Else you get the log in screen.
 func main() {
@@ -29,12 +88,8 @@ func main() {
 		log.Fatal("Could not dial up au service, %v", err)
 	}
 	auClient := api.NewAuthenticationClient(auConnection)
-	ctx := context.Background()
-	req := &api.LoginRequest{Username: "admin", Password: "kakakaka"}
-	rsp, err := auClient.Login(ctx, req)
-	if err != nil {
-		panic("Got error: " + err.Error())
-	}
-	log.Println(rsp)
-
+	connections := infrastructure.CreateConnection(auClient)
+	logger := log.New(os.Stdout, "INFO: ", log.Ltime|log.Ldate|log.Lshortfile)
+	server := &server{connections: connections, logger: logger}
+	http.ListenAndServe("0.0.0.0:8000", server)
 }
