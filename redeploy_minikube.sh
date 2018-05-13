f127a36861a33817fc860a443c0501d0a04549fe
David Tennander <david.tennander@gmail.com>
2018-05-13 23:24:08 +0200

    Moves docker setup to redeploy script.

--- redeploy_minikube.sh
+++ redeploy_minikube.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 
+# Set docker to point to the minikube daemon
+eval $(minikube docker-env --shell bash)
+function unsetDockerVarables {
+    eval $(minikube docker-env --shell bash -u)
+}
+trap unsetDockerVarables EXIT
+
 # Make all images
 cd authentication
 make
--- setup_minikube.sh
+++ setup_minikube.sh
@@ -24,14 +24,6 @@ openssl req -new -x509 -sha256 \
 
 minikube start
 
-# Set docker to point to the minikube daemon
-eval $(minikube docker-env --shell bash)
-function unsetDockerVarables {
-    deleteTempDir
-    eval $(minikube docker-env --shell bash -u)
-}
-trap unsetDockerVarables EXIT
-
 kubectl create namespace spexflix
 kubectl create secret tls tls-secret --cert=${DIR}/server.crt --key=${DIR}/server.key -n spexflix
 
