b81f37df17e66ddec209147f93348fef9ec1ff20
David Tennander <david.tennander@gmail.com>
2018-06-17 23:08:47 +0200

    Updates year view in GUI.

--- web/src/components/MovieView.js
+++ web/src/components/MovieView.js
@@ -2,12 +2,49 @@ import React, {Component} from 'react';
 import jwtDecode from 'jwt-decode'
 import Api from "../api";
 
-const headerStyle = {
-    margin: "20px",
+const movieHeaderStyle = {
+    width: "100%",
+    height: "350px",
+    maxHeight: "50%",
+    objectFit: "cover",
+    objectPosition: "50% 10%",
+};
+
+const gradientStyle = {
+    width: "100%",
+    height: "100%",
+    position: "absolute",
+    bottom: "0px",
+    left: "0px",
+    background: "linear-gradient(rgba(0,0,0,0), black)",
+};
+
+const titleStyle = {
+    after:"",
+    position: "absolute",
+    bottom: "8px",
+    left: "16px",
+    fontSize: "2em",
+    color: "#dbcd30",
 };
 
 const MovieHeader = (props) => {
-    return null;
+    console.log(props);
+    return (
+        <div style={{position: "relative"}}>
+            <img src={props.yearData.poster_uri} style={movieHeaderStyle} alt={props.yearData.name}/>
+            <div style={gradientStyle}/>
+            <div style={titleStyle}>
+                {props.yearData.name} <br/>
+                <i>eller {props.yearData.eller}</i>
+            </div>
+        </div>
+);
+};
+
+const movieStyle = {
+    width: "200px",
+    boxShadow: "0 16px 18px 0 rgba(0,0,0,0.12), 0 19px 25px 0 rgba(0,0,0,0.09)",
 };
 
 const Movies = (props) => {
@@ -15,11 +52,14 @@ const Movies = (props) => {
     for (let i in props.movies) {
         const movie = props.movies[i];
         views.push(
-            <div key={i}>
-                <video controls mediaGroup="video" src={movie.uri} style={{width:"200px"}}/>
-                <p>
-                    {movie.description}
-                </p>
+            <div key={i} style={{display: "flex", flexDirection: "row"}}>
+                <div >
+                    <h3 style={{margin: "10px 0px"}}>{movie.name}</h3>
+                    <video style={movieStyle} controls mediaGroup="video" src={movie.uri}/>
+                    <p>
+                        asdasdasd{movie.description}
+                    </p>
+                </div>
             </div>
         )
     }
@@ -34,6 +74,7 @@ class MovieView extends Component{
             user: {},
             tokenData: {},
             movies: [],
+            yearData: {},
         }
     }
 
@@ -41,6 +82,14 @@ class MovieView extends Component{
         try {
             const tokenData = jwtDecode(this.props.token);
             this.setState({tokenDate: tokenData});
+            Api.GetAllYears(this.props.token)
+                .then(allYears => {
+                    return allYears.filter(year =>
+                        year.year === "" + this.props.year)[0]
+                })
+                .then(year => {
+                    this.setState({yearData: year})
+                });
             Api.GetMovies(this.props.year, this.props.token)
                 .then(movies => this.setState({movies: movies}));
         } catch(error) {
@@ -50,10 +99,12 @@ class MovieView extends Component{
 
     render() {
         return (
-            <div style={headerStyle}>
-                <MovieHeader token={this.props.token} year={this.props.year}/>
-                <p>{this.props.description}</p>
-                <Movies movies={this.state.movies}/>
+            <div>
+                <MovieHeader yearData={this.state.yearData}/>
+                <div style={{margin: "0px 20px"}}>
+                    <p>{this.state.yearData.description}</p>
+                    <Movies movies={this.state.movies}/>
+                </div>
             </div>
         )
     }
